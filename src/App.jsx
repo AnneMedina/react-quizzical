3794414896ab78d2dc4b173c71f24d99d049624d
Anne Medina <carmelaanne.medina@gmail.com>
2024-11-19 14:04:08 +1030

    Worked on getting dynamic data from open api so I can see different possible values while working. The more scenarios, the better

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import Blueblob from "./assets/blueblob.png";
+import Yellowblob from "./assets/yellowBlob.png";
 import Startpage from "./components/Start";
 import Questions from "./components/Questions";
 import Answers from "./components/Answers";
@@ -7,18 +9,36 @@ import Answers from "./components/Answers";
 export default function App() {
   const [page, setPage] = React.useState(2);
 
-  const styles = { left: "-70px", bottom: "-50px" };
+  // const styles = { left: "-70px", bottom: "-50px" };
 
   function startGame() {
     console.log("start game");
     setPage(2);
   }
 
+  function checkAnswers() {}
+
   return (
     <main className="main-container">
+      <img className="yellow-blob" src={Yellowblob}></img>
       {page === 1 && <Startpage startGame={startGame} />}
-      {page === 2 && <Questions styles={styles} />}
-      {page === 3 && <Answers startGame={startGame} styles={styles} />}
+      {page === 2 && <Questions />}
+      {page === 3 && <Answers startGame={startGame} />}
+
+      <button
+        onClick={page == 2 ? checkAnswers : startGame}
+        className={page > 1 ? "action-button" : "start-button"}
+      >
+        {page == 1 ? "Start Quiz" : page === 2 ? "Check Answers" : "Play Again"}
+      </button>
+      <img
+        className="blue-blob"
+        src={Blueblob}
+        style={{
+          left: page > 1 ? "-70px" : "0px",
+          bottom: page > 1 ? "-50px" : "0px",
+        }}
+      />
     </main>
   );
 }
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,42 +1,40 @@
 import React from "react";
-import Yellowblob from "../assets/yellowblob.png";
-import Blueblob from "../assets/blueblob.png";
 import Trivias from "../data";
 import { nanoid } from "nanoid";
 
-export default function Questions(props) {
+export default function Questions() {
   const [questions, setQuestions] = React.useState(Trivias.trivias);
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data.results))
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setQuestions(Trivias.trivias);
+      });
+  }, []);
 
   console.log(questions);
 
   let list = questions.map((q) => (
     <div key={nanoid()} className="quiz-item">
-      <h4 className="questions">{q.question}</h4>
-      <label style={{ backgroundColor: "#d6dbf5" }}>
-        {q.correct_answer}
-        <input type="radio" name="answer" value={q.correct_answer} />
-      </label>
-      <label>
-        {q.incorrect_answers[1]}
-        <input type="radio" name="answer" value={q.incorrect_answers[1]} />
-      </label>
-      <label>
-        {q.incorrect_answers[1]}
-        <input type="radio" name="answer" value={q.incorrect_answers[2]} />
-      </label>
-      <label>
-        {q.incorrect_answers[1]}
-        <input type="radio" name="answer" value={q.incorrect_answers[3]} />
-      </label>
+      <h4 className="question-item">{q.question}</h4>
+      <div className="question-answers">
+        {q.incorrect_answers.map((incorrect, index) => (
+          <label key={index}>
+            {incorrect}
+            <input type="radio" name="answer" value={q.incorrect_answers[0]} />
+          </label>
+        ))}
+        <label style={{ backgroundColor: "#d6dbf5" }}>
+          {q.correct_answer}
+          <input type="radio" name="answer" value={q.correct_answer} />
+        </label>
+      </div>
       <hr />
     </div>
   ));
 
-  return (
-    <section className="questions">
-      <img className="yellow-blob" src={Yellowblob} />
-      {list}
-      <img className="blue-blob" src={Blueblob} style={props.styles} />
-    </section>
-  );
+  return <section className="questions">{list}</section>;
 }
